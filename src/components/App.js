6480d8a18d9bdb460462b7f19464177afaa53cc6
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2023-01-18 08:06:35 +0530

    modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CleanPlugin } from 'webpack';
 
 const App = () => {
+  const [relativelist,setRelativeList]= useState([
+    "Uncle","Aunty","Neice","Nephew","Aunty",
+  ]);
+
   return (
     <div id="main">
                {/* Do not remove the main div */}
+<ol>
+  {relativelist.map((relativelist,indx)=>{
+    
+    console.log(relativelist+indx);
+    return <li key={relativelist+indx}> Name : {relativesname}</li>
+  })}
+</ol>
     </div>
   )
 }
